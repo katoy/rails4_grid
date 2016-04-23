2dfc72ee8f0668c84c3f863ecf54e4fd0767451b
kato_youichi <youichikato@gmail.com>
2016-04-23 19:06:44 +0900

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails4Grid::Application.config.secret_token = 'b84aabb1bb058dda499a9602fa6f87b4cb0f1792bf96fa5ff8e8f4a4840a2eb1b803200de2b74c698be1d2c4dee83c931f084e708dfe358f784252f2f3dd13da'
